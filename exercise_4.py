a8986a43ce6f13cf4b0597fc45abf9951218b5b9
DimChatzMAI76 <mai20076@uom.edu.gr>
2024-09-26 01:28:03 +0300

    added 1 extra exercise

---
+++ exercise_4.py
@@ -0,0 +1,13 @@
+x=int(input('give the x: '))
+y=int(input('give the y: '))
+z=int(input('give the z: '))
+maximum=x
+if y>maximum:
+    maximum=y
+if z>maximum:
+    maximum=z    
+
+print(maximum)    
+
+
+
